fcbad3c3cb718bc3abb133cdcadf3b3f3a434dd3
davidscld <33543418+davidscld@users.noreply.github.com>
2018-11-14 10:17:44 +0100

    Creado y definidp metodo crearArrayListDeCartas

--- src/memorion/david/eva/Logica.java
+++ src/memorion/david/eva/Logica.java
@@ -14,9 +14,10 @@ import javax.swing.JPanel;
  * @author dvdsa
  */
 public class Logica {
+
     private ArrayList<Carta> ArrayListCartas = new ArrayList<>();
 
-    public void abrirFrameNiveles() {
+    public void abrirVistaMenuDificultad() {
         VistaMenuDificultad vistaMenuDificultad = new VistaMenuDificultad();
         vistaMenuDificultad.setVisible(true);
     }
@@ -28,7 +29,35 @@ public class Logica {
 
     public ArrayList<Carta> getArrayListCartas() {
         return ArrayListCartas;
-}
-    
-    
+    }
+
+    public void crearArrayListDeCartas() {
+       Carta carta1 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo1.jpg"));
+        Carta carta2 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo2.jpg"));
+        Carta carta3 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo3.jpg"));
+        Carta carta4 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo4.jpg"));
+        Carta carta5 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo5.jpg"));
+        Carta carta6 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo6.jpg"));
+        Carta carta7 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo7.jpg"));
+        Carta carta8 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo8.jpg"));
+        Carta carta9 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo9.jpg"));
+        Carta carta10 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo10.jpg"));
+        Carta carta11 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo11.jpg"));
+        Carta carta12 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo12.jpg"));
+        Carta reverso = new Carta(this.getClass().getResource("/assets/Cartas/reverso.jpg"));
+        ArrayListCartas.add(carta1);
+        ArrayListCartas.add(carta2);
+        ArrayListCartas.add(carta3);
+        ArrayListCartas.add(carta4);
+        ArrayListCartas.add(carta5);
+        ArrayListCartas.add(carta6);
+        ArrayListCartas.add(carta7);
+        ArrayListCartas.add(carta8);
+        ArrayListCartas.add(carta9);
+        ArrayListCartas.add(carta10);
+        ArrayListCartas.add(carta11);
+        ArrayListCartas.add(carta12);
+        ArrayListCartas.add(reverso);
+    }
+
 }
