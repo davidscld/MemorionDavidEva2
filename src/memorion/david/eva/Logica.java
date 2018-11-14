3032be331ecbc049b47e5f2dc0ac5bdb26e18635
davidscld <33543418+davidscld@users.noreply.github.com>
2018-11-14 20:00:51 +0100

    Declarado metodo abrir partida

--- src/memorion/david/eva/Logica.java
+++ src/memorion/david/eva/Logica.java
@@ -15,8 +15,8 @@ import javax.swing.JPanel;
  */
 public class Logica {
 
-    private ArrayList<Carta> ArrayListCartas = new ArrayList<>();
-    private ArrayList<Jugador> ArrayListJugador = new ArrayList<>();
+    private static ArrayList<Carta> ArrayListCartas = new ArrayList<>();
+    private static ArrayList<Jugador> ArrayListJugador = new ArrayList<>();
     
     public void abrirVistaMenuDificultad() {
         VistaMenuDificultad vistaMenuDificultad = new VistaMenuDificultad();
@@ -28,8 +28,11 @@ public class Logica {
         vistaRankingJugadores.setVisible(true);
     }
     
-    public void abrirPartida(){
-        
+    public void abrirPartida(String dificultad){
+        System.out.println("entre jaja");
+        Partida partida = new Partida(dificultad);
+        VistaJuegoCartas vistaJuegoCartas = new VistaJuegoCartas(partida);
+        vistaJuegoCartas.setVisible(true);
     }
 
     public ArrayList<Carta> getArrayListCartas() {
@@ -42,19 +45,19 @@ public class Logica {
     
 
     public void crearArrayListDeCartas() {
-       Carta carta1 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo1.jpg"));
-        Carta carta2 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo2.jpg"));
-        Carta carta3 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo3.jpg"));
-        Carta carta4 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo4.jpg"));
-        Carta carta5 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo5.jpg"));
-        Carta carta6 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo6.jpg"));
-        Carta carta7 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo7.jpg"));
-        Carta carta8 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo8.jpg"));
-        Carta carta9 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo9.jpg"));
-        Carta carta10 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo10.jpg"));
-        Carta carta11 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo11.jpg"));
-        Carta carta12 = new Carta(this.getClass().getResource("/assets/Cartas/lazarillo12.jpg"));
-        Carta reverso = new Carta(this.getClass().getResource("/assets/Cartas/reverso.jpg"));
+        Carta carta1 = new Carta(this.getClass().getResource("/assets/lazarillo1.jpg"));
+        Carta carta2 = new Carta(this.getClass().getResource("/assets/lazarillo2.jpg"));
+        Carta carta3 = new Carta(this.getClass().getResource("/assets/lazarillo3.jpg"));
+        Carta carta4 = new Carta(this.getClass().getResource("/assets/lazarillo4.jpg"));
+        Carta carta5 = new Carta(this.getClass().getResource("/assets/lazarillo5.jpg"));
+        Carta carta6 = new Carta(this.getClass().getResource("/assets/lazarillo6.jpg"));
+        Carta carta7 = new Carta(this.getClass().getResource("/assets/lazarillo7.jpg"));
+        Carta carta8 = new Carta(this.getClass().getResource("/assets/lazarillo8.jpg"));
+        Carta carta9 = new Carta(this.getClass().getResource("/assets/lazarillo9.jpg"));
+        Carta carta10 = new Carta(this.getClass().getResource("/assets/lazarillo10.jpg"));
+        Carta carta11 = new Carta(this.getClass().getResource("/assets/lazarillo11.jpg"));
+        Carta carta12 = new Carta(this.getClass().getResource("/assets/lazarillo12.jpg"));
+        Carta reverso = new Carta(this.getClass().getResource("/assets/reverso.png"));
         ArrayListCartas.add(carta1);
         ArrayListCartas.add(carta2);
         ArrayListCartas.add(carta3);
