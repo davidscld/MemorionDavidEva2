2071b867b5bfc4559c5bdf8b82d967b82358d329
davidscld <33543418+davidscld@users.noreply.github.com>
2018-11-14 12:03:19 +0100

    Definidos los metodos crearListado y crearPanelCentral

--- src/memorion/david/eva/VistaRankingJugadores.java
+++ src/memorion/david/eva/VistaRankingJugadores.java
@@ -1,11 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Esta clase ha de recibir el ArrayList que se encuentre en la clase logica del tipo Jugador.
+Se crearan siempre dos columnas y habra tantas filas como tamaño tenga el ArrayList.
+Recorreremos nuestro ArrayList y a medida , iremos introduciendo el JLabel el nombre y numero de movimientos
+Cada JLabel se añadira al Panel que haya dentro de esta clase
  */
 package memorion.david.eva;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
@@ -14,9 +16,11 @@ import javax.swing.JFrame;
 public class VistaRankingJugadores extends JFrame {
 
     private MenuSuperior menuSuperior;
-    
+    private Logica logica;
+    private JPanel panelCentral;
     public VistaRankingJugadores() {
         configurarVentana();
+        crearListado();
         configuracionMenuSuperior();
     }
 
@@ -24,11 +28,23 @@ public class VistaRankingJugadores extends JFrame {
         this.setSize(720, 905);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
+        crearPanelCentral();
 
     }
     
     private void configuracionMenuSuperior(){
         menuSuperior=new MenuSuperior();
         this.setJMenuBar(menuSuperior);
+    }
+
+    private void crearListado() {
+        logica = new Logica();
+        
+    }
+
+    private void crearPanelCentral() {
+        panelCentral = new JPanel();//Hay que asignarle su gridLayout
+        panelCentral.setBounds(50, 50, 500, 700);
+        this.add(panelCentral);
     }
 }
