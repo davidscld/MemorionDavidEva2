99ac5db3cac1ca37d642ce4f2048b190f61c2bce
evachico <33875153+evachico@users.noreply.github.com>
2018-11-23 09:16:34 +0100

    Comentarios clases

--- src/memorion/david/eva/ControladorPartidasGuardadas.java
+++ src/memorion/david/eva/ControladorPartidasGuardadas.java
@@ -16,11 +16,20 @@ import javax.swing.JLabel;
 public class ControladorPartidasGuardadas extends MouseAdapter {
 
     Logica logica;
-
+    /**
+     * ControladorPartidasGuardadas(Logica logica)
+     * El controlador se crea en la vistaMEnuPrincipal y esta envia la logica.
+     */
     public ControladorPartidasGuardadas(Logica logica) {
         this.logica = logica;
     }
-
+    /**
+     * public void actionPerformed(ActionEvent ae)
+     * te envia la opcion del menu que has pulsado 
+     * Llama a un metodo de la logica que se llama
+     * cargarPartidaGaurdada enviandole el nombre de la partida que quiero 
+     * cargar.
+     */
     @Override
     public void mouseClicked(MouseEvent me) {
 
--- src/memorion/david/eva/VistaRankingJugadores.java
+++ src/memorion/david/eva/VistaRankingJugadores.java
@@ -19,6 +19,8 @@ import javax.swing.JPanel;
 /**
  *
  * @author dvdsa
+ * 
+ * clase que extiende de JPanel
  */
 public class VistaRankingJugadores extends JPanel {
 
@@ -29,26 +31,45 @@ public class VistaRankingJugadores extends JPanel {
     private URL url = getClass().getResource("/assets/fondoPartidasJugadas.jpg");
     private Image imagenFondo = new ImageIcon(url).getImage();
     private Font f = new Font("Cambria", Font.ITALIC, 30);
-
+    /**
+     * VistaRankingJugadores(Logica logica)
+     * Constructor que se ejecuta en la logica cuando creamos la clase 
+     * VistaRankingJugador y nos envia la logica.
+     * Este constructor llama a los metodos configurarVentana() y crearListado()
+     */
     public VistaRankingJugadores(Logica logica) {
         this.logica = logica;
         configurarVentana();
         crearListado();
 
     }
-
+    /**
+     * configurarVentana()
+     * Metodo que da un tamaño al Jpanel de la clase y pone el layout a null
+     */
     private void configurarVentana() {
         this.setSize(720, 905);
         this.setLayout(null);
 
     }
-
+    /**
+     * paint(Graphics g)
+     * Metodo que se utiliza para poner la imagen de fondo dandole un tamaño
+     */
     public void paint(Graphics g) {
         g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
         this.setOpaque(false);
         super.paint(g);
     }
-
+    
+    /**
+     * crearListado()
+     * Compruebo si no esta vacio el array de jugador. Si no esta vacio
+     * recojo y asigno la cantidad de filas que habra, sabiendo el numero de 
+     * jugadores que han jugado.
+     * Luego se recorre el araryList y a medida se va sacando el nombre y numero
+     * de movimientos del juegador y los voy mostrnado.
+     */
     private void crearListado() {
 
         if (!logica.getArrayListJugador().isEmpty()) {
@@ -70,7 +91,13 @@ public class VistaRankingJugadores extends JPanel {
         }
 
     }
-
+    /**
+     * crearPanelCentral(int numeroDeFilas)
+     * Metodo que añade un Jpanel central en el que le doy un tamaño y las 
+     * coordenadas donde debe ir situado este panel que es el que va a llevar
+     * los JLabel de los jugadores que hay y se le añado a la clase ya que
+     * es un JPanel.
+     */
     private void crearPanelCentral(int numeroDeFilas) {
         panelCentral = new JPanel(new GridLayout(numeroDeFilas, 0, 5, 5));//Hay que asignarle su gridLayout
         panelCentral.setBounds(250, 10, 250, 50);
